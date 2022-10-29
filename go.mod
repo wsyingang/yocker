8f45d6bb19e6c15ed03775521c894162a2a109f7
wsyingang <1356931781@qq.com>
2022-10-29 22:08:06 +0800

    cli learn

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module yocker
 
 go 1.16
+
+require (
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/urfave/cli v1.22.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+)
 
-
-func main(){
-
+func main() {
+	app := cli.App{}
+	app.Name = "hello world"
+	app.Usage = "usage"
+	var m_port int
+	// add flag
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:        "port,p",
+			Value:       8080,
+			Usage:       "listen port",
+			Destination: &m_port,
+		},
+	}
+	// Action is the default command for app
+	//
+	app.Action = func(c *cli.Context) {
+		fmt.Println("func")
+		port := c.Int("port")
+		fmt.Printf("port=%d\n", port)
+	}
+	// Command use
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "calc 1+1",
+			Action: func(c *cli.Context) error {
+				fmt.Println("add func")
+				return nil
+			},
+			Subcommands: []cli.Command{
+				{
+					Name:  "addS",
+					Usage: "sub command for add",
+					Action: func(c *cli.Context) {
+						fmt.Println("sub command")
+					},
+				},
+			},
+		},
+	}
+	app.Run(os.Args)
 }
